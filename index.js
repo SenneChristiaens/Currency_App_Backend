33ac4111eac2da2235e8761b18fcc44929d82ffa
Bram-Colleman <r0820751@student.thomasmore.be>
2022-05-06 14:29:30 +0200

    chore: remove old database connections

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const transactionsRouter = require('./routers/transactions');
 const mongoose = require('mongoose');
 const cors = require('cors');
 // mongoose.connect('mongodb://localhost:27017/');
-// mongoose.connect('mongodb+srv://admin:admin@cluster0.flad1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-// mongoose.connect('mongodb+srv://admin:QGlzn6kVrqwk9ZpP@lab5.rnlkz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-// mongoose.connect('mongodb+srv://admin:QGlzn6kVrqwk9ZpP@lab5.rnlkz.mongodb.net/CurrencyApp?retryWrites=true&w=majority');
-// mongoose.connect('mongodb+srv://BramColleman:admin@lab5.rnlkz.mongodb.net/transactions?retryWrites=true&w=majority');
-// mongoose.connect('mongodb+srv://BramColleman:admin@lab5.rnlkz.mongodb.net/currencyapp?retryWrites=true&w=majority');
-// mongoose.connect('mongodb+srv://admin:admin@cluster0.flad1.mongodb.net/currencyApp?retryWrites=true&w=majority');
 mongoose.connect('mongodb+srv://admin:admin@backend.flad1.mongodb.net/currencyapp?retryWrites=true&w=majority');
 
 const app = express();
